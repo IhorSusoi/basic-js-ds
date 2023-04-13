bac217e21d5f73e8f704ecd21ded6e549d0c2183
Ihor Susoi <ihor.susoi@gmail.com>
2023-04-13 22:17:25 +0300

    add find

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
@@ -8,24 +8,76 @@ const { NotImplementedError } = require('../extensions/index.js');
 */
 class BinarySearchTree {
 
+  constructor(){
+    this.rootEl=null;
+  }
+
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.rootEl;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(value) {
+
+    this.rootEl = addWithin(this.rootEl, value);
+
+    function addWithin(node, value){
+
+      if(!node){
+        return new Node (value);
+      }
+
+      if(node.data === node){
+        return node;
+      }
+
+      if(value < node.data){
+        node.left = addWithin(node.left, value);
+      } else {
+        node.right = addWithin(node.right, value);
+      }
+
+      return node;
+    }
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(value) {
+    return searchWithin(this.rootEl, value);
+
+    function searchWithin(node, value){
+      if(!node){
+        return false;
+      }
+
+      if(node.data === value){
+        return true;
+      }
+
+      if(value < node.data){
+        return searchWithin(node.left, value);
+      } else {
+        return searchWithin(node.right, value);
+      }
+    }
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  find(value) {
+    return findWithin(this.rootEl, value);
+
+    function findWithin(node, value){
+      if(!node){
+        return null;
+      }
+
+      if(node.data === value){
+        return node;
+      }
+
+      if(value < node.data){
+        return findWithin(node.left, value);
+      } else {
+        return findWithin(node.right, value);
+      }
+    }
   }
 
   remove(/* data */) {
